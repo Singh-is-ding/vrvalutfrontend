9992ad20ef3b9317c1ec6e4ab1d3b06a14e9a70b
Singh-is-ding <adityarawat9368771@gmail.com>
2026-07-02 02:13:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import PlayerPage from "./pages/PlayerPage.jsx";
-import LibraryPage from "./pages/LibraryPage.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 export default function App() {
@@ -11,7 +10,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/player" element={<PlayerPage />} />
-        <Route path="/library" element={<LibraryPage />} />
       </Routes>
     </div>
   );
